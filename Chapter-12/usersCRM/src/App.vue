cba4a7f2c909c72044926e22cad0f79858e49f89
Sergey Shevtsov <sergeyprostoas@gmail.com>
2019-12-13 17:39:44 +0300

    Chapter-12 finish

--- Chapter-12/usersCRM/src/App.vue
+++ Chapter-12/usersCRM/src/App.vue
@@ -4,9 +4,9 @@
       <div class="navbar">
         <div class="container">
           <div class="navbar-content">
-            <div class="logo">VUE-CLI</div>
+            <div class="logo">Users CRM</div>
             <ul class="navbar-list">
-              <li class="navbar-item" v-for="link in links" :key="link.title">
+              <li v-for="link in links" :key="link.title" class="navbar-item">
                 <router-link
                   class="navbar-link"
                   :title="link.title"
--- Chapter-12/usersCRM/src/views/Users.vue
+++ Chapter-12/usersCRM/src/views/Users.vue
@@ -3,13 +3,128 @@
     <section>
       <div class="container">
         <h1>Users page</h1>
+        <!-- Table -->
+        <table>
+          <!-- Table Head -->
+          <thead>
+            <tr>
+              <th @click="sort('name')">Name &#8595;</th>
+              <th @click="sort('age')">Age &#8595;</th>
+              <th @click="sort('gender')">Gender &#8595;</th>
+            </tr>
+          </thead>
+
+          <!-- Table Body -->
+          <tbody>
+            <tr v-for="user in usersSort" :key="user.id">
+              <td>
+                <img :src="user.img" :alt="user.name" />
+                <span>{{ user.name }}</span>
+              </td>
+              <td>{{ user.age }}</td>
+              <td>{{ user.gender }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div style="text-align: center;">
+          <span>debug: sort: {{ currentSort }}, dir: {{ currentSortDir }}</span>
+          <p>page: {{ this.page.current }}, length: {{ this.page.length }}</p>
+        </div>
+      </div>
+    </section>
+
+    <!-- Button -->
+    <section>
+      <div class="container">
+        <div class="button-list">
+          <div class="btn btnPrimary" @click="prevPage">&#8592;</div>
+          <div class="btn btnPrimary" @click="nextPage">&#8594;</div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      users: [],
+      currentSort: 'name',
+      currentSortDir: 'asc',
+      page: {
+        current: 1,
+        length: 3
+      }
+    }
+  },
+
+  computed: {
+    usersSort() {
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      return this.users
+        .sort((a, b) => {
+          let mod = 1
+          if (this.currentSortDir === 'desc') mod = -1
+          if (a[this.currentSort] < b[this.currentSort]) return -1 * mod
+          if (a[this.currentSort] > b[this.currentSort]) return 1 * mod
+          return 0
+        })
+        .filter((row, index) => {
+          let start = (this.page.current - 1) * this.page.length
+          let end = this.page.current * this.page.length
+          if (index >= start && index < end) return true
+        })
+    }
+  },
+
+  created() {
+    axios
+      .get('https://api.myjson.com/bins/rzgya')
+      .then(response => {
+        // console.log(response.data)
+        this.users = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+
+  methods: {
+    sort(e) {
+      if (e === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc'
+      }
+      this.currentSort = e
+    },
+    // Pagination
+    prevPage() {
+      if (this.page.current > 1) this.page.current -= 1
+    },
+    nextPage() {
+      if (this.page.current * this.page.length < this.users.length)
+        this.page.current += 1
+    }
+  }
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+img {
+  width: 60px;
+  height: auto;
+  border-radius: 50%;
+  margin-right: 16px;
+}
+
+.button-list {
+  width: 100%;
+  text-align: center;
+
+  .btn {
+    margin: 0 20px;
+  }
+}
+</style>
