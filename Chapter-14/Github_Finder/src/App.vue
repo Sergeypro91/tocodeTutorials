7467abc3b359e7c9ab766b12146673a4c7744247
Sergeyprostoas <Sergeyprostoas@gmail.com>
2019-12-21 13:45:43 +0300

    Chapter-14 Finish

--- Chapter-14/Github_Finder/src/App.vue
+++ Chapter-14/Github_Finder/src/App.vue
@@ -5,16 +5,6 @@
         <div class="container">
           <div class="navbar-content">
             <div class="logo">Github Finder</div>
-            <!-- <ul class="navbar-list">
-              <li class="navbar-item" v-for="link in links" :key="link.title">
-                <router-link
-                  class="navbar-link"
-                  :title="link.title"
-                  :to="link.url"
-                  >{{ link.title }}</router-link
-                >
-              </li>
-            </ul> -->
           </div>
         </div>
       </div>
--- Chapter-14/Github_Finder/src/views/Home.vue
+++ Chapter-14/Github_Finder/src/views/Home.vue
@@ -1,15 +1,115 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
     <section>
+      <!-- Search -->
       <div class="container">
-        <h1>Home page</h1>
+        <!-- Serch input -->
+        <search
+          :value="search"
+          placeholder="Type username"
+          @search="search = $event"
+        />
+
+        <!-- Error -->
+        <div v-if="error" class="error">
+          <p>{{ error }}</p>
+        </div>
+
+        <!-- Search button -->
+        <button v-if="!repos" class="btn btnPrimary" @click="getRepos">
+          Search
+        </button>
+        <button v-else class="btn btnPrimary" @click="getRepos">
+          Search again!
+        </button>
+
+        <!-- Repo list -->
+        <div v-if="repos" class="repos__wrapper">
+          <!-- Repo item -->
+          <div v-for="repo in repos" :key="repo.id" class="repo-item">
+            <div class="repos-info">
+              <a class="link" target="_black" :href="repo.html_url">{{
+                repo.name
+              }}</a>
+              <span>{{ repo.stargazers_count }} ⭐</span>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import search from '@/components/Search.vue'
+
+export default {
+  components: { search },
+
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus()
+      }
+    }
+  },
+
+  data() {
+    return {
+      search: '',
+      error: null,
+      repos: null
+    }
+  },
+
+  methods: {
+    getRepos() {
+      axios
+        .get(`https://api.github.com/users/${this.search}/repos`)
+        .then(res => {
+          console.log(res)
+
+          this.repos = res.data
+          this.error = null
+        })
+        .catch(err => {
+          console.log(err)
+
+          this.repos = null
+          this.error = 'Can`t find this User'
+        })
+    }
+  }
+}
 </script>
 
-<style></style>
+<style lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+button {
+  margin-top: 40px !important;
+}
+
+.repos__wrapper {
+  width: 400px;
+  margin: 30px 0;
+}
+
+.repos-info {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding: 10px 0;
+  border-bottom: 1px solid #dbdbdb;
+}
+
+.error {
+  margin-top: 20px;
+}
+</style>
