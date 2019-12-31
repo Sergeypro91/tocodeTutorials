5b2de573687fedec35622e268d0c5c0e4f7257eb
Sergeyprostoas <Sergeyprostoas@gmail.com>
2019-12-31 18:42:48 +0300

    Change DZ directori DZ3

---
+++ Chapter-9/NotesAppStore/src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="message">
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ Chapter-9/NotesAppStore/src/store/notes.js
@@ -0,0 +1,12 @@
+export default {
+  state: {
+    title: 'Notes App'
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    getTitle(state) {
+      return state.title
+    }
+  }
+}
